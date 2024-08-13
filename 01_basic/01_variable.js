51bf453e54944a18573e25db8acd9ea0d138727f
Abhi-sha11 <abhishekkhandal219@gmail.com>
2024-08-13 05:16:44 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=1444141
+let accountEmail="abhishek@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 not allowed
+accountEmail="hdfsdf"
+accountPassword="22647464"
+accountCity="Benguluru"
+console.log(accountId)
+console.table([accountId, accountEmail, 
+    accountPassword, accountState, accountCity])
